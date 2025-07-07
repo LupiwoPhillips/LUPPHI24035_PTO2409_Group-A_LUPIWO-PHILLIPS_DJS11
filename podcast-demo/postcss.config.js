09a90ebe9a00d92147f92c52556fa85c2f2d989c
LupiwoPhillips <lupiwonp@gmail.com>
2025-07-07 21:09:18 +0200

    updates
    Tailwind css configuration

---
+++ podcast-demo/postcss.config.js
@@ -0,0 +1,8 @@
+// postcss.config.js
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
+// This configuration file sets up PostCSS with Tailwind CSS and Autoprefixer for a React project.
